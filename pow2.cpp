589b773d36e9f8a54b08a13aeeec54c454dbca41
Punyaslok Dutta <punyaslokdutta@gmail.com>
2021-11-10 23:26:35 +0530

    Create pow2.cpp

---
+++ pow2.cpp
@@ -0,0 +1,26 @@
+#define ll long long 
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+       double temp;
+       if(n==0)
+       {
+           return 1;
+       }
+       temp=myPow(x, n/2);
+       if(n%2==0)
+       {
+           return temp* temp;
+       }
+        else
+        {
+             if(n > 0)
+            return x * temp * temp;
+        else
+            return (temp * temp) / x;// to handle floats and negative n
+        }
+      
+        
+    }
+};
