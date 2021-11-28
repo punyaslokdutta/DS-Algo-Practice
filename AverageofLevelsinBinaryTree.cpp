62606e1b77856d1b100d1e838863b44e7c33d329
Punyaslok Dutta <punyaslokdutta@gmail.com>
2021-11-28 23:15:08 +0530

    Create AverageofLevelsinBinaryTree.cpp

---
+++ AverageofLevelsinBinaryTree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size = q.size();
+            double sum = 0;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                sum += node->val;
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            ans.push_back(sum/size);
+        }
+        return ans;
+    }
+};
