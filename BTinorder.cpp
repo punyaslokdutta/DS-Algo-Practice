3e3433051b00fa0f956d220eb4a20907d56283f8
Punyaslok Dutta <punyaslokdutta@gmail.com>
2021-12-09 13:01:21 +0530

    Update BTinorder.cpp

--- BTinorder.cpp
+++ BTinorder.cpp
@@ -1,27 +1,110 @@
 //Iterative
-class Solution{
-    public:
-    vector<int> inorderTraversal(TreeNode* root)
-    {
-        vector<int> res;
-        stack<TreeNode*> st;
-        TreeNode* curr=root;
-        while(curr!=NULL || !st.empty())
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//Inorder -> {left, root, right};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//Inorder -> {left, root, right};
+class Solution {
+public:
+    vector<int> answer;
+    // void inorder(TreeNode * root)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     inorder(root->left);
+    //     answer.push_back(root->val);
+    //     inorder(root->right);
+    // }
+    
+    vector<int> inorderTraversal(TreeNode* root) { 
+       stack<TreeNode*> st;
+       TreeNode* p=root;
+       vector<int> ans;
+        while(p!=NULL || !st.empty())
         {
-            while(curr!=NULL)
+            if(p!=NULL)
             {
-                st.push(curr);
-                curr=curr->left;
+                st.push(p);
+                p=p->left;
             }
-            curr=st.top();
-            st.pop();
-            res.push_back(curr->val);
-            curr=curr->right;
+            else
+            {
+              p=st.top();
+              st.pop();
+              ans.push_back(p->val);
+              p=p->right; // Take out from stack to find the last parent and move to it's right
+            }
         }
-        return res;
+        return ans;
     }
+       
+   
 };
 
+
+    
+    
+    
+    
+    
+    
+//Recursive
+// vector<int> inorderTraversal(TreeNode* root) { 
+//         inorder(root);
+//         return ans;
+//     }
+//     void inorder(TreeNode* root )
+//     {
+//         if(root==NULL)
+//             return;
+//         inorder(root->left);
+//         ans.push_back(root->val);
+//         inorder(root->right);
+//     }
+    
+    
+    
+    
+    
+    
+//Recursive
+// vector<int> inorderTraversal(TreeNode* root) { 
+//         inorder(root);
+//         return ans;
+//     }
+//     void inorder(TreeNode* root )
+//     {
+//         if(root==NULL)
+//             return;
+//         inorder(root->left);
+//         ans.push_back(root->val);
+//         inorder(root->right);
+//     }
 
 
 //Recursive
