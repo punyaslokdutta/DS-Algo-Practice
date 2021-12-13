93953452ec5a6a8af4e71cd06b70ec10c3d9fbad
Punyaslok Dutta <punyaslokdutta@gmail.com>
2021-12-13 12:44:15 +0530

    Update CourseSchedule.cpp

--- CourseSchedule.cpp
+++ CourseSchedule.cpp
@@ -2,6 +2,7 @@
 //TOPOLOGICAL SORT TO FIND CYCLE
 //(a, b)-> To complete A , you need to complete B. 
 //B--> A , cause B is a depencency of A.
+//INDEGREE 0 MEANS NO OTHER COURSE IS DEPENDENT ON THEM AND THEY CAN COMPLETE THEMSELVES
 
 class Solution {
 public:
