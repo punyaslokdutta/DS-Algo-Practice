31041015610b71c5e962dde16e2c0580a7d14b59
Punyaslok Dutta <punyaslokdutta@gmail.com>
2021-12-15 14:12:35 +0530

    Update uniquePath.cpp

--- uniquePath.cpp
+++ uniquePath.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    long long fact(int n){
-        if(n==1 || n==0) return 1;
-        return fact(n-1)*n;
+    
+    long long fact(int x)
+    {
+        if(x==1 || x==0)
+            return 1;
+        return x* fact(x-1);
     }
-    int uniquePaths(int m, int n) {
-        
-        int x=min(m,n);
-        int y=max(m,n);
-        y=m+n-2;
+    
+    int uniquePaths(int m, int n) { 
+        //IF WE TAKE MAX HERE , THE LOOP TIMINGS WILL INCREASE BOTH FOR WHILE 
+        // AND FACTORIAL RECURSION.
+        int x=min(m, n); 
+        int y=m+n-2;
         int t=x-1;
         long long res=1;
-        while(t--){
+
+        while(t--)
+        {
             res=res*y;
             y--;
         }
-        long long z=fact(x-1);
         
-        return res/z;
+        long long l=fact(x-1);
+        
+        return res/l;
     }
+   
 };
