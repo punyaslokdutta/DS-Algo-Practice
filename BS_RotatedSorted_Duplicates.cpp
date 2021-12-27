1508235442ee00d3826da207474391c6b609cb46
Punyaslok Dutta <punyaslokdutta@gmail.com>
2021-12-27 20:15:57 +0530

    Create BS_RotatedSorted_Duplicates.cpp

---
+++ BS_RotatedSorted_Duplicates.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool doesExist(vector<int>& nums,int l, int h, int target)
+    {
+        if(l>h)
+            return false;
+        int mid= l+ (h-l)/2;
+        if(nums[mid]==target)
+            return true;
+      
+        //CONDITION TO HANDLE WHEN DUPLICATE ELEMENTS ARE PRESENT IN SORTED ROTATED ARRAY
+        if(nums[mid]==nums[l] && nums[l]==nums[h])
+        {
+            l++;
+            h--;
+            return doesExist(nums, l, h, target);
+        }
+        //left part is sorted
+        if(nums[mid]>=nums[l])
+        {
+            if(nums[mid]>=target && nums[l]<=target)
+                return doesExist(nums, l, mid-1, target);
+            return doesExist(nums, mid+1, h, target);
+        }
+        //right part is sorted  
+            if(target>=nums[mid] && nums[h]>=target)
+                return doesExist(nums, mid+1, h, target);
+            return doesExist(nums, l, mid-1, target);
+            
+    }
+    bool search(vector<int>& nums, int target) {
+        int h= nums.size()-1;
+        int l=0;
+        return doesExist(nums, l, h, target);
+        
+    }
+};
