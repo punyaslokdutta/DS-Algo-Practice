f6a7e111cc47cb13341991fa181019588ac7e4c2
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-02-23 02:42:22 +0530

    Time: 3 ms (88.23%), Space: 44 MB (10.26%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,36 +1,28 @@
-public class Solution {
-    private TrieNode root = new TrieNode();
-    public boolean wordBreak(String str, List<String> wordDict) {
-        for (String word : wordDict)
-            addToTrie(word);
-        boolean[] dp = new boolean[str.length() + 1];
-        char[] s = str.toCharArray();
-        dp[0] = true;
-        for (int i = 0; i < s.length; i++) {
-            if (!dp[i])
-                continue;
-            int j = i; // start trie traversal
-            TrieNode cur = root;
-            while (j < s.length && cur.children[s[j] - 'a'] != null) {
-                cur = cur.children[s[j++] - 'a'];
-                if (cur.isWord)
-                    dp[j] = true;
-            }
-        }
-        return dp[str.length()];
+class Solution{
+	
+	public boolean wordBreak(String s, List<String> wordDict) {
+	// for memoization
+        HashMap<String,Boolean> map= new HashMap<>();
+        return canConstruct(s, wordDict, map);
     }
+    
+	public boolean canConstruct(String target, List<String> words, HashMap<String,Boolean> mp)
+	{
+		if(mp.containsKey(target))
+			return mp.get(target);
+        
+        if(target.isEmpty())
+            return true;
+		for(String word: words)
+		{
+			if(target.startsWith(word) && canConstruct(target.substring(word.length()), words, mp))
+			{
+				mp.put(target, true);
+				return true;
+			}
+		}
 
-    private void addToTrie(String word) {
-        TrieNode cur = root;
-        for (char ch : word.toCharArray()) {
-            if (cur.children[ch - 'a'] == null)
-                cur.children[ch - 'a'] = new TrieNode();
-            cur = cur.children[ch - 'a'];
-        }
-        cur.isWord = true;
-    }
-    class TrieNode {
-        public boolean isWord;
-        public TrieNode[] children = new TrieNode[26];
-    }
+		mp.put(target, false);
+		return false;
+	}
 }
