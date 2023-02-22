5a828db59971fe1e930a801d4b1dfd18096e9912
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-02-23 03:07:21 +0530

    Time: 22 ms (5.48%), Space: 53.7 MB (5.67%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,28 +1,22 @@
-class Solution{
-	
-	public boolean wordBreak(String s, List<String> wordDict) {
-	// for memoization
-        HashMap<String,Boolean> map= new HashMap<>();
-        return canConstruct(s, wordDict, map);
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int index = 0;
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(s);
+        Set<String> visited = new HashSet<String>();
+        while(!queue.isEmpty()){
+            String candidate = queue.poll();
+            if(wordDict.contains(candidate)) return true;
+            for(int i = 0; i < candidate.length(); i++){
+                String chop = candidate.substring(0,i);
+                String next = candidate.substring(i, candidate.length());
+                if(!visited.contains(next) && wordDict.contains(chop)){
+                    next = candidate.substring(i, candidate.length());
+                    queue.offer(next);
+                    visited.add(next);
+                }
+            }
+        }
+        return false;
     }
-    
-	public boolean canConstruct(String target, List<String> words, HashMap<String,Boolean> mp)
-	{
-		if(mp.containsKey(target))
-			return mp.get(target);
-        
-        if(target.isEmpty())
-            return true;
-		for(String word: words)
-		{
-			if(target.startsWith(word) && canConstruct(target.substring(word.length()), words, mp))
-			{
-				mp.put(target, true);
-				return true;
-			}
-		}
-
-		mp.put(target, false);
-		return false;
-	}
 }
