eaf406e5e465ae40d8eaf46784998eaff6942108
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-03-02 10:48:47 +0530

    Time: 11 ms (51.41%), Space: 42.4 MB (51.16%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,30 @@
+
+// egge , adda
+//e-> a , 
+
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        Map<Character, Character> mp = new HashMap<>();
+        for (int i = 0;i<s.length();i++)
+        {
+            //System.out.println(mp);
+            if(!mp.containsKey(s.charAt(i)) )
+            {
+                if(mp.containsValue(t.charAt(i)))
+                    return false;
+                mp.put(s.charAt(i), t.charAt(i));
+            }
+            else
+            {
+                if(mp.get(s.charAt(i))!= t.charAt(i))
+                    return false;
+                else
+                    continue;
+            }
+        } 
+        return true;
+    }
+}
