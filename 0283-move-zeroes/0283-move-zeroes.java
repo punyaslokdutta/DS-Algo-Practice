c9a2d0a492110eb33eb10daaab7df9abb6e290bc
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-04-03 07:48:55 +0530

    Time: 1 ms (100.00%), Space: 43.9 MB (54.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-    if(nums.length == 0)
-        return;
-    int insertPos = 0;
-    for(int num: nums)
-    {
-    	if(num!= 0)
-    	{
-    		nums[insertPos++]= num;
-    	} 
-    }
-    while(insertPos<nums.length)
-    {
-    nums[insertPos++]= 0;
-    }
+        int insertPos = 0;
+        int lastNonZero;
+        for(int i = 0; i<nums.length;i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[insertPos++] = nums[i];
+            }
+        }
+        
+        while(insertPos < nums.length)
+        {
+            nums[insertPos++] = 0;
+        }
+    
     }
 }
