58c96f80928d580240e6985c2924f05601199251
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-04-12 12:56:11 +0530

    Time: 6 ms (97.73%), Space: 46.4 MB (30.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> mp = new HashMap<>();
-        for(String s : strs)
+       Map<String, List<String>> mp = new HashMap<>();
+        for(String str : strs)
         {
-            char [] ch = new char [26];
-            for(char c: s.toCharArray()) ch[c - 'a']++;
-            String temp = String.valueOf(ch);
-            if(!mp.containsKey(temp)) 
-                mp.put(temp, new ArrayList<>());
-            mp.get(temp).add(s);
+            char [] ch = new char[26];
+            for(char c :  str.toCharArray())
+            {
+                ch[c-'a']++;
+            }
+            String tmp = String.valueOf(ch);
+            if(!mp.containsKey(tmp))
+                mp.put(tmp, new ArrayList<>());
+            mp.get(tmp).add(str);
         }
         
         return new ArrayList<>(mp.values());
-        
-        
     }
 }
