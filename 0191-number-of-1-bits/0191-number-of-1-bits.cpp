efa424659331a1e8d1595c1d22846f535980b251
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-04-13 13:52:42 +0530

    Time: 2 ms (51.97%), Space: 5.9 MB (74.46%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,9 +4,9 @@ public:
         int ans=0;
         while(n!=0)
         {
-            if(n&1 == 1)
+            if((n&1) == 1)
                 ans++;
-            n = n/2;
+            n = n>>1;
         }
         return ans;
     }
