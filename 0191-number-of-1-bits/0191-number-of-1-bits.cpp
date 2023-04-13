df3f551eb1df90d6329349657459c52bdc9dbbf5
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-04-13 13:51:16 +0530

    Time: 5 ms (27.74%), Space: 6 MB (74.46%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans=0;
+        while(n!=0)
+        {
+            if(n%2 == 1)
+                ans++;
+            n = n/2;
+        }
+        return ans;
+    }
+    
+};
