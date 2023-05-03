b0bb03114da659e4ed8dcdabe178d69f8c1eb8e5
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-05-03 22:54:10 +0530

    Time: 50 ms (24.99%), Space: 51.3 MB (28.68%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,27 +1,6 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            new MaxComparator()
-        );
-        int m = points.length;
-        int n = points[0].length;
-        for(int[] point : points)
-        {
-            pq.add(point);
-            if(pq.size() > k)
-            {
-                pq.poll();
-            }
-        }
-        
-        return pq.toArray(new int[0][]);
-    }
-    
-    private static class MaxComparator implements Comparator<int[]>
-    {
-        public int compare(int a[], int b[])
-        {
-            return (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]);
-        }
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+        return Arrays.copyOfRange(points, 0, K);
     }
 }
