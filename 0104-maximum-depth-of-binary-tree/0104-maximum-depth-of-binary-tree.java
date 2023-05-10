bad1c02d26f6855f376016950b1cd620998676d1
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-05-10 23:58:09 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (85.07%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,12 +13,11 @@
  *     }
  * }
  */
-class Solution
-{
-public int maxDepth(TreeNode root) {
-        if(root==null){
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
             return 0;
-        }
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+        return 1+  Math.max(maxDepth(root.left),  maxDepth(root.right));
+        
     }
 }
