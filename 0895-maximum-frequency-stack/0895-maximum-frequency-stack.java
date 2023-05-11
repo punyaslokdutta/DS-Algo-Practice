7ed519792b0bb737c763ebbc9667845437b97d7e
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-05-12 02:02:02 +0530

    Time: 38 ms (89.32%), Space: 51.2 MB (81.07%) - LeetHub

--- 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
@@ -1,26 +1,42 @@
+
+//freqMap 
+//List<List<Integer>>
+// [1, 2, 3, 4, 6, 7, 8]
+//1 -> [2, 4, 5] 
+//8 -> [5, 6, 2] return 5 
+// List<Map.Entry<Integer, Integer>> [] buckets
+
+
 class FreqStack {
-    HashMap<Integer, Integer> fmap;
-    List<Stack<Integer>> stack;
+    Map<Integer, Integer> fMap;
+    List<Stack<Integer>> st;
     
     public FreqStack() {
-        fmap = new HashMap();
-        stack = new ArrayList();
-        stack.add(new Stack());
+        fMap = new HashMap<>();
+        st = new ArrayList<>();
+        st.add(new Stack<>());
+        
     }
     
-    public void push(int x) {
-        int freq = fmap.getOrDefault(x, 0) + 1;
-        fmap.put(x, freq);
-        if (freq == stack.size()) stack.add(new Stack());
-        stack.get(freq).add(x);
+    public void push(int val) {
+        int freq = fMap.getOrDefault(val, 0);
+        freq = freq + 1;
+        fMap.put(val,  freq);
+        if(freq  == st.size())
+        {
+            st.add(new Stack<>());
+        }
+        st.get(freq).push(val);
     }
-
+    
     public int pop() {
-        Stack<Integer> top = stack.get(stack.size()-1);
-        int x = top.pop();
-        if (top.size() == 0) stack.remove(stack.size()-1);
-        fmap.put(x, fmap.get(x) - 1);
-        return x;
+        int maxFreq = st.size() -1;
+        Stack<Integer> top = st.get(maxFreq);
+        int ans = top.pop();
+        fMap.put(ans, fMap.get(ans) - 1);
+        if(top.isEmpty()) st.remove(maxFreq);
+        return ans;
+        
     }
 }
 
