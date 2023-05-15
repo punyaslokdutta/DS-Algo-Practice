f45f060727d8879fbdde7859b94c27245671a36c
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-05-15 15:02:06 +0530

    Time: 5 ms (95.02%), Space: 42.4 MB (56.91%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,17 +1,23 @@
+
+//Custom comaprator to rearrange the integers in array
+//(b+a).compareTo(a + b) -> descending order
 class Solution {
     public String largestNumber(int[] nums) {
-        String [] arr = new String[nums.length];
-        int idx=0;
-        for(int x: nums)
+        int n = nums.length;
+        String []arr = new String[n];
+        int idx =0;
+        for(int x : nums)
         {
             arr[idx++] = Integer.toString(x);
         }
-        
-        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
-        if(arr[0].charAt(0)== '0') return "0";
+        Arrays.sort(arr, (a, b ) -> (b+a).compareTo(a+b));
+        if(arr[0].charAt(0) == '0') return "0";
         StringBuilder sb = new StringBuilder();
-        for(String s:arr) sb.append(s);
+        for(String x: arr) sb.append(x);
         return sb.toString();
+        
         
     }
 }
+    
+    //sort 900 digits -> 900log900
