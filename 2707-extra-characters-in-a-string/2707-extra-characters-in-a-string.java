40885c09d7b82ca1c4e179c1bc652fff50cb60ec
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-04 20:40:34 +0530

    Time: 46 ms (80.56%), Space: 44.2 MB (82.27%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,26 +1,26 @@
 class Solution {
     Set<String> st;
-    int dp[];
-    int helper(String s,int idx){
-        if(idx >= s.length()) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        StringBuilder sb = new StringBuilder();
+    Map<String, Integer> mp;
+    int helper(String s){
+        if(s.length() == 0 )  return 0;
+        if(mp.containsKey(s)) return mp.get(s);
         int ans =s.length() + 1;
-        for(int i = idx;i < s.length();i++){
-            sb.append(s.charAt(i));
-            if(st.contains(sb.toString())){
-                ans = Math.min(ans,helper(s,i + 1));
+        for(String word : st)
+        {
+            if(s.startsWith(word))
+            {
+                ans = Math.min(ans, helper(s.substring(word.length())));
             }
         }
-        ans = Math.min(ans, 1 + helper(s,idx + 1));
+        ans = Math.min(ans, 1 + helper(s.substring(1)));
         
-        return dp[idx] = ans;
+        mp.put(s, ans);
+        return ans;
     }
     public int minExtraChar(String s, String[] dictionary) {
         st = new HashSet<>();
-        dp = new int[s.length()];
-        Arrays.fill(dp,-1);
+        mp = new HashMap<>();
         for(var d:dictionary) st.add(d);
-        return helper(s,0);
+        return helper(s);
     }
 }
