87a29914a8f8d6aaae37aa3fc424c34b3ec8cee5
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-07 15:16:20 +0530

    Time: 3 ms (73.19%), Space: 43.6 MB (63.52%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int []dp = new int[n];
+        Arrays.fill(dp , 1);
+        for(int i=1;i<n;i++)
+        {
+           if(ratings[i-1] < ratings[i]){
+               dp[i] = dp[i-1] + 1;
+           }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i+1]  < ratings[i] )
+            {
+                dp[i] = Math.max(dp[i], dp[i+1] + 1);
+            }
+        }
+        int sum =0;
+        for(int x: dp) sum+=x;
+        return sum;
+        
+    }
+}
+
+
+// [1,2,87,87,87,2,1]
+// 1, 1, 2, 2, 87, 87, 87
+// 1  1  2  2  3   3   3
+//sorting wont work , cause of adjacency
+
+
