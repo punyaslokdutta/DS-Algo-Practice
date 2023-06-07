12e445f2d6aedf33f4acf3f9ff97a462447468cf
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-07 16:47:07 +0530

    Time: 41 ms (19.95%), Space: 51.4 MB (18.33%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,44 +1,58 @@
-class Solution{
-    public boolean possibleBipartition(int n, int[][] dislikes) {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-    int[] color = new int[n + 1];
-    boolean isBi = true;
-
-    for (int i = 1; i <= n; i++) {
-        graph.put(i, new ArrayList<>());
-    }
-
-    for (int[] dislike : dislikes) {
-        int u = dislike[0];
-        int v = dislike[1];
-        graph.get(u).add(v);
-        graph.get(v).add(u);
-    }
-
-    for (int i = 1; i <= n; i++) {
-        if (color[i] == 0) {
-            color[i] = 1;
-            if (!dfs(i, graph, color)) {
+class Solution {
+    Map<Integer, List<Integer>> graph;
+    int[]color;
+    boolean isBi;
+    void dfs(int node)
+    {
+        if(!graph.containsKey(node))
+            return;
+        if(graph.get(node).size() == 0)
+            return;
+        for(int v: graph.get(node))
+        {
+            if(color[v] == 0)
+            {
+                color[v] = -color[node];
+                dfs(v);
+            }
+            else if(color[v]!=0 && (color[v] == color[node]))
+            {
                 isBi = false;
-                break;
+                return;
             }
         }
     }
-
-    return isBi;
-}
-
-private boolean dfs(int node, Map<Integer, List<Integer>> graph, int[] color) {
-    for (int neighbor : graph.get(node)) {
-        if (color[neighbor] == 0) {
-            color[neighbor] = -color[node];
-            if (!dfs(neighbor, graph, color)) {
-                return false;
-            }
-        } else if (color[neighbor] == color[node]) {
-            return false;
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        graph = new HashMap<>();
+        color = new int[n + 1];
+        isBi = true;
+        for(int i=0;i<dislikes.length;i++)
+        {
+            int u = dislikes[i][0];
+            int v = dislikes[i][1];
+            graph.putIfAbsent(u, new ArrayList<>());
+            graph.putIfAbsent(v, new ArrayList<>());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+            
         }
+        
+        Arrays.fill(color , 0); //unvisted;
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i] == 0)
+            {
+                color[i] = 1;
+                dfs(i);
+            }
+        }
+        
+        
+        return isBi;
+        
+        
+        
+        
+        
     }
-    return true;
 }
-};
