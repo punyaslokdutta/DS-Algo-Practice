1dc70e5b87135013dc5b73a0292dde3db2a14652
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-14 17:51:20 +0530

    Time: 132 ms (28.86%), Space: 44.2 MB (28.94%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,39 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int []fmap = new int[150];
-        for(char c: t.toCharArray())
+        int [] mp = new int[150];
+        for(char c : t.toCharArray())
         {
-            fmap[c]++;
+            mp[c]++;
         }
-        
-        int left=0;
-        int right=0;
-        int minWindowLen = s.length()+1;
-        int charsInWindow =0;
-        int allReqChars = t.length();
+        int l = 0;
+        int r = 0;
+        int n = s.length();
+        int charsReq = t.length();
+        int charsInWindow = 0;
         String ans = "";
-        while(right < s.length())
+        int minLen = n+1;
+        while(r<n)
         {
-            char curr = s.charAt(right);
-            fmap[curr]--;
-            if(fmap[curr]>=0) charsInWindow++;
-            while(charsInWindow == allReqChars)
+            char ch = s.charAt(r);
+            mp[ch]--;
+            if(mp[ch] >=0 ) charsInWindow++;
+            while( charsInWindow == charsReq )
             {
-                int windowLen = right - left +1;
-                if(windowLen < minWindowLen)
-                {
-                    minWindowLen = windowLen;
-                    ans = s.substring(left, right + 1);
-                }
-                
-                fmap[s.charAt(left)]++;
-                if(fmap[s.charAt(left)] > 0) charsInWindow--;
-                left++;
-                
+                 char leftCh = s.charAt(l);
+                 int len = r - l + 1;
+                 if(len < minLen)
+                 {
+                     minLen = len;
+                     ans = s.substring(l, r+1);
+                 }
+                 mp[leftCh]++;
+                 if(mp[leftCh] > 0) charsInWindow--;
+                 l++;   
             }
-            right++;
+            r++;
         }
-        return ans;
         
+        return ans;
     }
 }
