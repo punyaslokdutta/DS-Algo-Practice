47249dcc6a470b81cae327efb6eb359c33f911ea
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-23 01:26:38 +0530

    Time: 1117 ms (11.98%), Space: 44 MB (41.94%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int beautifulSubsets(int[] nums, int k) {
+        Map<Integer, Integer> fMap = new HashMap<>();
+        return generateSubsets(nums ,0, fMap, k) -1;
+    }
+    
+    public int generateSubsets(int []a ,int index,  Map<Integer, Integer> fMap, int k)
+    { 
+        if(index == a.length) return 1;
+        int res = generateSubsets(a , index + 1, fMap,  k);
+        if(!fMap.containsKey(a[index] + k) && !fMap.containsKey(a[index] - k))
+        {
+            //can add this a[index]
+            fMap.put(a[index], fMap.getOrDefault(a[index], 0) + 1);
+            res+=generateSubsets(a, index + 1, fMap, k);
+            fMap.put(a[index], fMap.get(a[index]) -1);
+            if(fMap.get(a[index]) == 0) fMap.remove(a[index]);
+        }
+        return res;
+    }
+}
+
+
+// [5, 3, 4, 10, 6, 3, 3] k = 3
+    
+// [5 ]
+// [5, 3]
+// [5, 3, 4]
+
+// 2^n 
+// 10^3 = 2^10 
+// 10^6
+
+// 2^20 
+
+// 2^n * nC2
