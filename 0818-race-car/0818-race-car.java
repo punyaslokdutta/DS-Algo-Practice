14660031f0f3ee16f52681db258576ba96becf8f
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-27 00:52:22 +0530

    Time: 312 ms (17.23%), Space: 43.4 MB (51.69%) - LeetHub

--- 0818-race-car/0818-race-car.java
+++ 0818-race-car/0818-race-car.java
@@ -5,20 +5,31 @@ class Solution {
         q.add(new int []{0, 1, 0});
         while(!q.isEmpty())
         {
+            //st.add(f);
+            
+            int sz = q.size();
+            for(int i=0;i<sz;i++)
+            {
             int []f = q.poll();
             int pos = f[0];
             int speed = f[1];
             int moves = f[2];
-            //st.add(f);
+            System.out.print(f[0]);
+            System.out.print(f[1]);
+            System.out.print(f[2]);
+            System.out.print("->");
             if(pos == target) return moves;
-            
-            //if(st.contains(new int[]{pos, speed , moves})) continue;
-            q.add(new int[]{pos + speed , 2*speed , moves + 1}); //A
+            q.add(new int[]{pos + speed , 2*speed , moves + 1});
             if((pos + speed > target && speed >0) || (pos + speed < target && speed < 0))
             {
                 //R
                 q.add(new int[]{pos, speed > 0 ? -1 : 1 , moves + 1 });
             }
+                
+            }
+            //if(st.contains(new int[]{pos, speed , moves})) continue;
+        
+            
             
         }
         return 0;
