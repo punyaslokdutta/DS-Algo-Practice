a552582f265daf928dcfe9c2f62cf1090f79f0b4
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-29 03:02:32 +0530

    Time: 1 ms (100.00%), Space: 60.4 MB (7.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,7 +4,7 @@ class Solution {
         int max = nums[0];
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (sum > max) max = sum;
+            max = Math.max(sum , max);
             if (sum < 0) sum = 0;
         }
         return max;
