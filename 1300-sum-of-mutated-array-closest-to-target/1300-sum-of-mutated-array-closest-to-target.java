9020b327ea6e56f2d2e879593031e0d2a9046e0f
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-06-29 08:52:54 +0530

    Time: 4 ms (64.52%), Space: 43.7 MB (13.71%) - LeetHub

---
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.java
@@ -0,0 +1,49 @@
+class Solution {
+    private int getModifiedSum(int m , int []arr)
+    {
+        int n = arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]  > m)
+            {
+                sum+=m;
+            }
+            else{
+                sum+=arr[i];
+            }
+        }
+        
+        return sum;
+    }
+    public int findBestValue(int[] arr, int target) {
+        int l = 0;
+        int r = Arrays.stream(arr).max().getAsInt();
+        int minDiff = Integer.MAX_VALUE;
+        int ans = 0;
+        while(l<=r)
+        {
+            int m = l + (r-l)/2;
+            int currSum = getModifiedSum(m, arr);
+            if( currSum > target)
+            {
+                r = m-1;
+            }
+            else{
+                l = m+1;
+            }
+            int currDiff = Math.abs(target - currSum);
+            if(currDiff < minDiff || (minDiff == currDiff  && m < ans))
+                                         {
+                                            ans = m;
+                                            minDiff = currDiff;
+                                         }
+        }
+        return ans;
+    }
+}
+
+
+// [4,9,3]
+
+// for a value of m , sum increasing 
