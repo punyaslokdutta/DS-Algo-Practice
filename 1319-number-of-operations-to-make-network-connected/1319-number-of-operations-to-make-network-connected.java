5397c0cc972a6d2ee85c5afa55f609e84e59db0c
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-07-03 00:37:35 +0530

    Time: 34 ms (9.51%), Space: 68.1 MB (21.77%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,39 @@
+class Solution {
+    Set<Integer> vis;
+    private void dfs(int node , Map<Integer, List<Integer>> graph)
+    {
+        vis.add(node);
+        for(int v : graph.get(node))
+        {
+            if(!vis.contains(v))
+            {
+                dfs(v, graph);
+            }
+        }
+    }
+    public int makeConnected(int n, int[][] connections) {
+        int edges = connections.length;
+        if(edges < n - 1) return -1;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        vis = new HashSet<>(); 
+        for(int i=0;i<n;i++) graph.put(i , new ArrayList<>());
+        for(int[]con : connections)
+        {
+            int node1 = con[0];
+            int node2 = con[1];
+            graph.get(node1).add(node2);
+            graph.get(node2).add(node1);
+        }
+        int numComp = 0;
+        for(int i=0;i<n ;i++)
+        {
+            if(!vis.contains(i))
+            {
+                dfs(i, graph);
+                numComp++;
+            }
+        }
+        
+        return numComp - 1;
+    }
+}
