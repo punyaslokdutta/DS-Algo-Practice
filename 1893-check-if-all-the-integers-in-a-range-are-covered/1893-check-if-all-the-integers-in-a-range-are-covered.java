28912ccf2ed014e95ee1208e3c2b2d2d7fabcce3
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-07-02 23:35:20 +0530

    Time: 2 ms (34.20%), Space: 40.6 MB (88.27%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
@@ -1,15 +1,30 @@
 class Solution {
     public boolean isCovered(int[][] ranges, int left, int right) {
-        int[]dp  = new int[60];
-        Arrays.fill(dp, 0);
-        for(int []range : ranges)
+        Arrays.sort(ranges, (a, b) -> a[0] - b[0]);
+        for(int[]range : ranges)
         {
-            for(int i=range[0];i<=range[1];i++) dp[i]++;
+            if(left>=range[0] && left<=range[1])
+            {
+                left = range[1] + 1;
+            }
         }
-        for(int i=left;i<=right;i++)
-        {
-            if(dp[i] == 0) return false;
-        }
-        return true; 
+        return left > right;
     }
 }
+
+//check the line sweep algo -> O(n + m)
+
+
+// bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+//     int line[52] = {};
+//     for (auto &r : ranges) {
+//         line[r[0]] += 1;
+//         line[r[1] + 1] -= 1;
+//     }
+//     for (int i = 1, overlaps = 0; i <= right; ++i) {
+//         overlaps += line[i];
+//         if (i >= left && overlaps == 0)
+//             return false;
+//     }
+//     return true;
+// }
