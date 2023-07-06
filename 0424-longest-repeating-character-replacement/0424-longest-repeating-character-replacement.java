2236a4e3eeb1e202a06a1dd3498ef8023839f19b
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-07-06 23:15:57 +0530

    Time: 30 ms (26.89%), Space: 43.7 MB (21.10%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,28 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         Map<Character, Integer> mp = new HashMap<>();
-        int i=0;
-        int n = s.length();
-        int maxChars = Integer.MIN_VALUE;
-        int longWindow = 0;
-        for(int j=0;j<n;j++)
+        int majorityCharCount = 0;
+        int len = s.length();
+        int maxWindow = 0;
+        int i = 0;
+        int j =0;
+        while(j<len)
         {
-            mp.put(s.charAt(j), mp.getOrDefault(s.charAt(j), 0) + 1);
-            maxChars = Math.max(maxChars, mp.get(s.charAt(j)));
-            while(j-i+1 - maxChars > k)
-                                {
-                                    char rm = s.charAt(i);
-                                    mp.put(rm, mp.getOrDefault(rm, 0) -1);
-                                    i++;
-                                }
-            longWindow = Math.max(longWindow, j-i+1);
+            char ch = s.charAt(j);
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+            majorityCharCount = Math.max(majorityCharCount, mp.get(ch));
+            if(j-i+1 - majorityCharCount > k)
+            {
+                char rm = s.charAt(i);
+                mp.put(rm , mp.get(rm) -1 );
+                i++;
+            }
+            maxWindow = Math.max(maxWindow, j - i + 1 ); 
+            j++;
         }
-                                
-        return longWindow;
+        return maxWindow;
+        
+        
     }
 }
-
-
-//Map -> maxChars n that window
-        //Replacement -> j-i+ 1 - maxChars > k, then shift start
-        //j - i + 1 - maxChars is the number of replacements that can be done.
