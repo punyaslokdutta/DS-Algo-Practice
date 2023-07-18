e444592fc86e224df3c9c73e54ccfaff9851f794
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-07-19 00:49:53 +0530

    Time: 26 ms (56.34%), Space: 45.2 MB (87.25%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,31 @@
+class Solution {
+    private List<List<Integer>> ans;
+    private int num;
+    private int kk;
+    private void solve(List<Integer> curr , int index , int []arr)
+    {
+        if(index == num){
+            if( curr.size() == kk){
+            ans.add(new ArrayList<>(curr));
+        }
+            return;
+        }
+        
+        
+        curr.add(arr[index]);
+        solve(curr , index + 1, arr);
+        curr.remove(curr.size() - 1);
+        solve(curr , index + 1, arr );
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> curr = new ArrayList<>();
+        // for(int i=1;i<=n;i++) curr.add(i);
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) arr[i] = i+1;
+        num = n;
+        kk = k;
+        ans = new ArrayList<>();
+        solve(curr, 0, arr);
+        return ans;
+    }
+}
