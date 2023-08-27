3ac4a434e41285e8cc3f2ba99b53960fba80e5b1
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-08-27 22:21:36 +0530

    Time: 54 ms (53.87%), Space: 58.9 MB (26.16%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,44 +1,46 @@
 class Solution {
-    private boolean solve(int []stones , Map<Integer, Integer> mp, int currPos , int lastJumpLen , int [][]dp )
-    {
-        int n = stones.length;
-        if(currPos == stones[n-1]) return true;
-        if(currPos > stones[n-1]) return false;
-        if(lastJumpLen < 1 ) return false; 
-        if(dp[mp.get(currPos)][lastJumpLen]!=-1) return dp[mp.get(currPos)][lastJumpLen] == 1;
-        boolean path1 = false;
-        boolean path2 = false;
-        boolean path3 = false;
-        boolean temp = false;
-        if(mp.containsKey(currPos + lastJumpLen - 1))
-        {
-            temp = temp || solve(stones , mp, currPos + lastJumpLen -1, lastJumpLen -1, dp);
+    HashMap<Integer, Integer> m = new HashMap<>();
+    int[][] dp;
+
+    boolean solve(int i, int k, int[] stones) {
+        if (i == stones.length - 1) {
+            return true;
         }
-        if(mp.containsKey(currPos + lastJumpLen))
-        {
-            temp = temp || solve(stones , mp, currPos + lastJumpLen, lastJumpLen, dp);
+
+        if (dp[i][k] != -1) {
+            return dp[i][k] == 1;
         }
-        if(mp.containsKey(currPos + lastJumpLen + 1))
-        {
-            temp = temp || solve(stones , mp, currPos + lastJumpLen + 1, lastJumpLen + 1 , dp);
+
+        boolean k0 = false, kp = false, k1 = false;
+
+        if (m.containsKey(stones[i] + k)) {
+            k0 = solve(m.get(stones[i] + k), k, stones);
         }
-        
-        int  ans = temp? 1: 0;
-        dp[mp.get(currPos)][lastJumpLen] = ans;
-        return ans == 1 ;
-        
-        
+        if (k > 1 && m.containsKey(stones[i] + k - 1)) {
+            kp = solve(m.get(stones[i] + k - 1), k - 1, stones);
+        }
+        if (m.containsKey(stones[i] + k + 1)) {
+            k1 = solve(m.get(stones[i] + k + 1), k + 1, stones);
+        }
+
+        dp[i][k] = (k0 || kp || k1) ? 1 : 0;
+        return dp[i][k] == 1;
     }
+
     public boolean canCross(int[] stones) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        for(int i=0;i<stones.length;i++) mp.put(stones[i], i);
-        int [][]dp = new int[2100][2100];
-        for(int i=0;i<2100;i++)
-        {
-            Arrays.fill(dp[i], -1);
+        if (stones[1] - stones[0] != 1) {
+            return false;
         }
-        if(stones[1] - stones[0] > 1) return false;
-        return solve(stones , mp, stones[1], 1, dp );
-        
+
+        for (int i = 0; i < stones.length; i++) {
+            m.put(stones[i], i);
+        }
+
+        dp = new int[stones.length][stones.length];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solve(1, 1, stones);
     }
 }
