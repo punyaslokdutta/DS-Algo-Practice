3f667d0b30c67dd099a3b3269c4bc1053e72f057
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-09-06 07:52:06 +0530

    Time: 87 ms (62.65%), Space: 107.9 MB (78.01%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int largestPathValue(String colors, int[][] edges) {
+        int n = colors.length();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i=0;i<n;i++) graph.put(i, new ArrayList<>());
+        int []indegree = new int[n];
+        Arrays.fill(indegree, 0);
+        for(int []edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            indegree[v]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int [][]memo = new int[n][26]; // memo[index][color] -> maxColor ending at index nod     
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i] == 0) 
+            {
+                q.offer(i);
+                char ch = colors.charAt(i);
+                memo[i][ch - 'a']++;
+            }
+        }
+        
+        
+        while(!q.isEmpty())
+        {
+            --n;
+            int curr = q.poll();
+            if(graph.get(curr).size() == 0)
+            {
+                ans = Math.max(ans, Arrays.stream(memo[curr]).max().getAsInt());
+            }
+            for(int adj : graph.get(curr))
+            {
+                for(int i=0;i<26;i++)
+                {
+                  memo[adj][i] = Math.max(memo[adj][i], memo[curr][i] + (i == colors.charAt(adj)-'a'? 1 : 0));
+                }
+                
+                if(--indegree[adj] == 0)
+                {
+                    q.offer(adj);
+                }
+            }
+        }
+        
+        return (n == 0) ? ans : -1;
+        
+    }
+}
