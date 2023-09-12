860d3a80fc3f69d49c28ccb54db303bb5bf0636a
Punyaslok Dutta <punyaslokdutta@gmail.com>
2023-09-12 23:22:44 +0530

    Time: 62 ms (88.88%), Space: 42.6 MB (16.77%) - LeetHub

--- 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -3,17 +3,16 @@
  * @return {Promise<any>}
  */
 var promiseAll = async function(functions) {
-    return await new Promise((resolve, reject) => {
-        // steps 1 and 2 of approach described above
+    return  new Promise((resolve, reject) => {
         let arr = Array(functions.length);
-        let waitingFor = functions.length;
-
-        // steps 3 and 4
-        for(let i = 0; i < functions.length; ++i){
+        let n = functions.length;
+        let count = 0;
+        for(let i = 0; i < n; ++i){
             functions[i]()
                 .then((result) => { 
                     arr[i] = result; 
-                    if (--waitingFor === 0) resolve(arr);
+                    count++;
+                    if(count === n ) resolve(arr);
                 }).catch(reject);
         }
     });
